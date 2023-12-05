9097aafd634a8600ee2f253ea112e7a74a28e462
masterdesky <masterdesky@gmail.com>
2023-12-05 02:58:15 +0100

    day 3 code golfed lol

--- 2023/day-3/part-1.py
+++ 2023/day-3/part-1.py
@@ -1,12 +1,14 @@
-import re
-import numpy as np
+from re import *
 def main():
-    n = lambda M: re.sub('[\d]', '1', M)
-    u = lambda M: re.sub('[.]', '0', M)
-    c = lambda M: re.sub('[^01\n]', '9', M)
+    s = lambda p, M: sub(p, '.', M).splitlines()
     with open('input.dat') as f:
-        M = [list(map(int, list(l))) for l in c(u(n(f.read()))).splitlines()]
-        M = np.pad(np.array(M), [(1, 1), (1, 1)])
-        C = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        D, K = s('[^\d\n]', L:=f.read()), s('[\d]', L)
+    h = ['.'*(w:=len(D[0])+2)]
+    p = lambda M: ''.join(h+[f'.{l}.' for l in M]+h)
+    d, k = p(D), p(K)
+    S = [(m.start(), m.end()) for m in finditer('[^.]+', d)]
+    C = [i=='1' for i in sub('[^.]', '1', k)]
+    c = lambda i: any([C[i+j] for j in[-w-1,-w,-w+1,-1,1,w-1,w,w+1]])
+    print(sum([int(d[slice(*r)]) for r in S if any([c(i) for i in range(*r)])]))
 if __name__ == '__main__':
     main()
