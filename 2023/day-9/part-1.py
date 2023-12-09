bf8f19bced2ffb981041dc8ea1180cb3cce27555
masterdesky <masterdesky@gmail.com>
2023-12-09 21:32:44 +0100

    small logic update to day9

--- 2023/day-9/part-1.py
+++ 2023/day-9/part-1.py
@@ -4,7 +4,7 @@ def main():
         L = [list(map(int, findall(r'-?\d+', l))) for l in f.read().splitlines()]
     N = [[] for _ in L]
     for i, l in enumerate(L):
-        while any([i != 0 for i in l]) and (len(l) > 0):
+        while any(l) and (len(l) > 0):
             N[i].append(l[-1])
             l = [a-b for a, b in zip(l[1:], l)]
     print(sum(sum(i for i in n) for n in N))
--- 2023/day-9/part-2.py
+++ 2023/day-9/part-2.py
@@ -4,9 +4,9 @@ def main():
         L = [list(map(int, findall(r'-?\d+', l))) for l in f.read().splitlines()]
     N = [[] for _ in L]
     for i, l in enumerate(L):
-        while any([i != 0 for i in l]) and (len(l) > 0):
+        while any(l) and (len(l)>0):
             N[i].append(l[0])
             l = [a-b for a, b in zip(l[1:], l)]
-    print(sum(sum(-v if i % 2 else v for i, v in enumerate(n)) for n in N))
+    print(sum(sum(-v if i%2 else v for i, v in enumerate(n)) for n in N))
 if __name__ == '__main__':
     main()
