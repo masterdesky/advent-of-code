94a7a5f3e7c29ff0e365508b0e1b602e3c0e1c63
masterdesky <masterdesky@gmail.com>
2023-12-12 01:14:31 +0100

    finished day 10 part 2

--- 2023/day-10/part-2.py
+++ 2023/day-10/part-2.py
@@ -1,20 +1,20 @@
-import sys
-class recursionlimit:
-    def __init__(self, limit):
-        self.limit = limit
-
-    def __enter__(self):
-        self.old_limit = sys.getrecursionlimit()
-        sys.setrecursionlimit(self.limit)
-
-    def __exit__(self, type, value, tb):
-        sys.setrecursionlimit(self.old_limit)
-def A(L, M, x):
-    if x < 0 or x >= len(L) or L[x] != 1:
-        return
-    L[x] = -1
-    for dx in M:
-        A(L, M, x+M[dx])
+def FF(L, M, x):
+    Q = [x]
+    while Q:
+        x = Q.pop(0)
+        if x < 0 or x >= len(L) or L[x] != 1: continue
+        L[x] = -1
+        for dx in M:
+            y = x + M[dx]
+            if 0 <= y < len(L) and L[y] == 1: Q.append(y)
+def RC(E, i, C):
+    c, xp, yp = 0, i%C, i//C
+    for e in E:
+        x1, y1, x2, y2 = e[0]%C, e[0]//C, e[1]%C, e[1]//C
+        if (yp < y1) != (yp < y2) and \
+            xp < x1 + (x2-x1)*(yp-y1)/(y2-y1):
+            c += 1
+    return c%2 != 1
 def main():
     P = {'|':('U','D'),'-':('L','R'),'L':('U','R'),
          'J':('L','U'),'7':('L','D'),'F':('R','D')}
@@ -28,12 +28,15 @@ def main():
     while (t:=T[-1]) != T[0]:
         d = s(t[0], P[L[t[1]]])
         T.append((D[d], t[1]+M[d]))
-    I = set([i for _, i in T][:-1])
-    L = [0 if i in I else 1 for i in range(len(L))]
-    with recursionlimit(5000):
-        A(L, M, 0)
-    L = ['F' if c==-1 else c for c in L]
-    with open('flood.dat', 'w') as f:
-        f.write('\n'.join([''.join([str(L[i*C+j]) for j in range(C)]) for i in range(R)]))
+    I = [i for _, i in T]
+    L = [0 if i in set(I) else 1 for i in range(len(L))]
+    FF(L, M, 0)
+    L = [0 if c==-1 else c for c in L]
+    E = [(i, j) for i, j in zip(I, I[1:])]
+    for i in [i for i in range(len(L)) if L[i] == 1]:
+        if RC(E, i, C): L[i] = 0
+    #with open('flood.dat', 'w') as f:
+    #    f.write('\n'.join([''.join(map(str, L[i*C:(i+1)*C])) for i in range(R)]))
+    print(sum(L))
 if __name__ == '__main__':
     main()
